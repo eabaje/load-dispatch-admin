eda8cada87b42677642f387470534c435e649272
Ebi Abaje <eabaje@outlook.com>
2022-01-24 00:21:42 +0100

    1-22-2022-1

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,9 +1,15 @@
 export const SIGN_UP = "/register";
 export const LOG_IN = "http://";
-export const API_URL = "http://localhost:8000/api/";
+//export const API_URL = "http://localhost:8000/api/";
 
-export const LOG_OUT = "http://localhost:8000/api/";
+export const API_URL = "https://loadboard-api.herokuapp.com/api/";
 
-export const IMG_URL = "http://localhost:8000/pics/";
+export const LOG_OUT = "https://load-dispatch-admin.herokuapp.com";
 
-export const DOC_URL = "http://localhost:8000/docs/";
+// export const IMG_URL = "http://localhost:8000/pics/";
+
+export const IMG_URL = "https://loadboard-api.herokuapp.com/pics/";
+
+//export const DOC_URL = "http://localhost:8000/docs/";
+
+export const DOC_URL = "https://loadboard-api.herokuapp.com/docs/";
