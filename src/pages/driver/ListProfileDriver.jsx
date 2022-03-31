b0a847dd2ce47125f17b0d4948ba7f9cbd4e23d0
Ebi Abaje <eabaje@outlook.com>
2022-03-31 13:03:24 +0100

    31-03-2022

--- src/pages/driver/ListProfileDriver.jsx
+++ src/pages/driver/ListProfileDriver.jsx
@@ -1,52 +1,43 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { Container, Row, Col } from 'react-grid';
+import { Container, Row, Col } from "react-grid";
 import { useSnackbar } from "notistack";
-import { API_URL } from '../../constants';
+import { API_URL } from "../../constants";
 import { fetchDataAll } from "../../helpers/query";
 function ListProfileDriver() {
-
   const { enqueueSnackbar, closeSnackbar } = useSnackbar();
- 
+
   const [data, setData] = useState([]);
   const [user, setUser] = useState({});
-  
+
   // Calling the function on component mount
   useEffect(() => {
-    fetchDataAll(
-      "driver/findAllAssignedDrivers"
-    
-    )((driver) => {
-
-      setData(driver.data);
-     
-
+    fetchDataAll("driver/findAllAssignedDrivers")((driver) => {
+      setData(driver);
     })((err) => {
       enqueueSnackbar(err.message, { variant: "error" });
     });
-    
+
     setUser(JSON.parse(localStorage.getItem("user")));
   }, []);
-
+  console.log("data", data);
   return (
-   <>
-     <Container fluid className="grid">
-        <Row>
-          {data.map((f, index) => (
-            <Col className="col-lg-4 ml-5">
-              <driverCard
-                data={f[index]}
-               
-              />
-            </Col>
-          ))}
-        </Row>
-      </Container>
-  
-   
-   
-   </>
-  )
+    <>
+      {data.length > 0 ? (
+        <Container fluid className="grid">
+          <Row>
+            {data.map((f, index) => (
+              <Col className="col-lg-4 ml-5">
+                <driverCard d={f[index]} />
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      ) : (
+        <h4 class="alert alert-info">No driver record found</h4>
+      )}
+    </>
+  );
 }
 
-export default ListProfileDriver
+export default ListProfileDriver;
