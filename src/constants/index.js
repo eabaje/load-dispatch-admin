0fdb7c43209c6daf8c7e784f5cdcf1a363619c72
ITSupport <ebi.abaje@gmail.com>
2022-04-20 18:59:01 +0100

    4-20-2022-3

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,8 +1,8 @@
 export const SIGN_UP = "/register";
 export const LOG_IN = "http://";
-export const API_URL = "http://localhost:8000/api/";
+//export const API_URL = "http://localhost:8000/api/";
 
-//export const API_URL = "https://loadboard-api.herokuapp.com/api/";
+export const API_URL = "https://loadboard-api.herokuapp.com/api/";
 
 export const LOG_OUT = "https://load-dispatch-admin.vercel.app";
 
