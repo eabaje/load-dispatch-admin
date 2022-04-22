7c17368962c72b6b178b4ad4875c9ad204d988ec
ITSupport <ebi.abaje@gmail.com>
2022-04-22 18:14:08 +0100

    4-22-2022

--- src/context/actions/auth/logoutUser.js
+++
@@ -1,9 +0,0 @@
-import { LOGOUT_USER } from "../../../constants/actionTypes";
-
-export default () => (dispatch) => {
-  localStorage.removeItem("token");
-  localStorage.removeItem("user");
-  dispatch({
-    type: LOGOUT_USER,
-  });
-};
