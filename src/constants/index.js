841f50f3152ef84ca6be7cf5c14d97e8a8b5adb7
ITSupport <ebi.abaje@gmail.com>
2022-04-29 18:17:15 +0100

    4-29-2022-1

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,28 +1,48 @@
 export const SIGN_UP = "/register";
 export const LOG_IN = "http://";
-//export const API_URL = "http://localhost:8000/api/";
 
-export const API_URL = "https://loadboard-api.herokuapp.com/api/";
+
+
+/* local host environment variables */
+
+export const API_URL = "http://localhost:8000/api/";
 
 export const LOG_OUT = "https://load-dispatch-admin.vercel.app";
 
-//export const IMG_URL = "http://localhost:8000/uploads/";
+export const IMG_URL = "http://localhost:8000/uploads/";
 
-//export const PIC_URL = "http://localhost:8000/uploads/pics/";
+export const PROFILE_IMG_URL = "http://localhost:8000/profiles/";
 
-export const IMG_URL = "https://loadboard-api.herokuapp.com/uploads/";
+export const VEHICLE_IMG_URL = "http://localhost:8000/vehicles/";
 
-export const PROFILE_IMG_URL = "https://loadboard-api.herokuapp.com/profiles/";
+export const SHIPMENT_IMG_URL = "http://localhost:8000/shipments/";
 
-export const VEHICLE_IMG_URL = "https://loadboard-api.herokuapp.com/vehicles/";
+export const PIC_URL = "http://localhost:8000/";
 
-export const SHIPMENT_IMG_URL = "https://loadboard-api.herokuapp.com/shipments/";
+export const DOC_URL = "http://localhost:8000/docs/"
 
-export const PIC_URL = "https://loadboard-api.herokuapp.com/";
+
+
+
+/* Remote host environment variables */
+
+//export const API_URL = "https://loadboard-api.herokuapp.com/api/";
+
+//export const LOG_OUT = "https://load-dispatch-admin.vercel.app";
+
+// export const IMG_URL = "https://loadboard-api.herokuapp.com/uploads/";
+
+// export const PROFILE_IMG_URL = "https://loadboard-api.herokuapp.com/profiles/";
+
+// export const VEHICLE_IMG_URL = "https://loadboard-api.herokuapp.com/vehicles/";
+
+// export const SHIPMENT_IMG_URL = "https://loadboard-api.herokuapp.com/shipments/";
+
+// export const PIC_URL = "https://loadboard-api.herokuapp.com/";
 
 //export const DOC_URL = "http://localhost:8000/docs/";
 
-export const DOC_URL = "https://loadboard-api.herokuapp.com/docs/";
+//export const DOC_URL = "https://loadboard-api.herokuapp.com/docs/";
 
 export const Public_Key = "pk_test_c06524a4666917095175d12761920ec03b4ebb35";
 
